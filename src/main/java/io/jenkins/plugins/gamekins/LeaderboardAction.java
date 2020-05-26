c293e97d80b34f253a36c59c0a22587f853233a5
Philipp Straubinger <ieee@straubinger.ph>
2020-05-26 16:27:06 +0200

    ADD: Fill leaderboard with users

--- src/main/java/io/jenkins/plugins/gamekins/LeaderboardAction.java
+++ src/main/java/io/jenkins/plugins/gamekins/LeaderboardAction.java
@@ -2,6 +2,13 @@ package io.jenkins.plugins.gamekins;
 
 import hudson.model.AbstractProject;
 import hudson.model.ProminentProjectAction;
+import hudson.model.User;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class LeaderboardAction implements ProminentProjectAction {
 
@@ -28,5 +35,60 @@ public class LeaderboardAction implements ProminentProjectAction {
     @Override
     public String getUrlName() {
         return "leaderboard";
+    }
+
+    public List<UserDetails> getUserDetails() {
+        ArrayList<UserDetails> details = new ArrayList<>();
+        for (User user : User.getAll()) {
+            GameUserProperty property = user.getProperty(GameUserProperty.class);
+            if (property != null && property.isParticipating()) {
+                details.add(
+                        new UserDetails(
+                                user.getFullName(),
+                                property.getTeamName(),
+                                property.getScore(),
+                                property.getAbsolvedChallenges().size()
+                        )
+                );
+            }
+        }
+        details.sort(Comparator.comparingInt(UserDetails::getScore));
+        return details;
+    }
+
+    @ExportedBean(defaultVisibility = 999)
+    public class UserDetails {
+
+        private String userName;
+        private String teamName;
+        private int score;
+        private int absolvedChallenges;
+
+        public UserDetails(String userName, String teamName, int score, int absolvedChallenges) {
+            this.userName = userName;
+            this.teamName = teamName;
+            this.score = score;
+            this.absolvedChallenges = absolvedChallenges;
+        }
+
+        @Exported
+        public String getUserName() {
+            return this.userName;
+        }
+
+        @Exported
+        public String getTeamName() {
+            return this.teamName;
+        }
+
+        @Exported
+        public int getScore() {
+            return this.score;
+        }
+
+        @Exported
+        public int getAbsolvedChallenges() {
+            return this.absolvedChallenges;
+        }
     }
 }
