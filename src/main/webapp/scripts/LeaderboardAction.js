d308412180c1a92cc6ea71b59d17d86ff7f18e44
rainerm <rainerm@fim.uni-passau.de>
2022-06-30 12:46:25 +0200

    UPDATE: removed redundancies in javascript files

--- src/main/webapp/scripts/LeaderboardAction.js
+++ src/main/webapp/scripts/LeaderboardAction.js
@@ -1,17 +1,3 @@
-window.onload = (function () {
-    prettyPrint();
-    jQuery3('[data-toggle="tooltip"]').tooltip()
-
-    let avatars = jQuery3('[id^=imageUser]')
-    let src = document.getElementsByTagName("img")[0].src
-    let base = src.substring(0, src.indexOf("static"))
-    let endSplit = src.substring(src.indexOf("static")).split("/")
-    for (let avatar of avatars) {
-        let split = avatar.src.split("/")
-        avatar.src = base + "static/" + endSplit[1] + "/plugin/gamekins/avatars/" + split[split.length - 1]
-    }
-});
-
 jQuery3("#closeModalBtn").on("click", () => {
     jQuery3("#error-text")[0].innerHTML = ""
     jQuery3("#reason-text").val("")
