5488e5471aed253be7a7bb7737e0cc651ae45878
Philipp Straubinger <philipp.straubinger@uni-passau.de>
2022-11-23 12:53:22 +0100

    UPDATE: Generation probability for MutationChallenges

--- src/main/kotlin/org/gamekins/GamePublisherDescriptor.kt
+++ src/main/kotlin/org/gamekins/GamePublisherDescriptor.kt
@@ -137,8 +137,7 @@ class GamePublisherDescriptor : BuildStepDescriptor<Publisher?>(GamePublisher::c
         challenges[LineCoverageChallenge::class.java] = 4
         challenges[MethodCoverageChallenge::class.java] = 3
         challenges[TestChallenge::class.java] = 1
-        //challenges[MutationChallenge::class.java] = 6
-        challenges[MutationChallenge::class.java] = 1000
+        challenges[MutationChallenge::class.java] = 6
         challenges[SmellChallenge::class.java] = 4
     }
 
