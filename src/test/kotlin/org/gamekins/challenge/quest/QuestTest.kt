b15b65fbf7db3d4922e8c4f756d47dab2ce6199f
rainerm <rainerm@fim.uni-passau.de>
2023-02-02 13:36:57 +0100

    UPDATE: QuestTest to FeatureSpec

--- src/test/kotlin/org/gamekins/challenge/quest/QuestTest.kt
+++ src/test/kotlin/org/gamekins/challenge/quest/QuestTest.kt
@@ -1,7 +1,7 @@
 package org.gamekins.challenge.quest
 
 import hudson.model.TaskListener
-import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.mockk.every
@@ -10,27 +10,26 @@ import io.mockk.unmockkAll
 import org.gamekins.challenge.LineCoverageChallenge
 import org.gamekins.util.Constants
 
-class QuestTest : AnnotationSpec() {
+class QuestTest : FeatureSpec({
 
-    private lateinit var quest: Quest
-    private val challenge1 = mockkClass(LineCoverageChallenge::class)
-    private val challenge2 = mockkClass(LineCoverageChallenge::class)
-    private val step1 = QuestStep("", challenge1)
-    private val step2 = QuestStep("", challenge2)
-    private lateinit var quest1: Quest
-    private val run = mockkClass(hudson.model.Run::class)
+    lateinit var noStepQuest: Quest
+    val challenge1 = mockkClass(LineCoverageChallenge::class)
+    val challenge2 = mockkClass(LineCoverageChallenge::class)
+    val step1 = QuestStep("", challenge1)
+    val step2 = QuestStep("", challenge2)
+    lateinit var twoStepQuest: Quest
+    val run = mockkClass(hudson.model.Run::class)
 
-    @BeforeEach
-    fun init() {
-        quest = Quest("Lines over lines", arrayListOf())
-        quest1 = Quest("Lines over Lines", arrayListOf(step1, step2))
-        every { challenge1.equals(any()) } returns true
+    beforeContainer {
+        noStepQuest = Quest("Lines over lines", arrayListOf())
+        twoStepQuest = Quest("Lines over Lines", arrayListOf(step1, step2))
+        every { challenge1 == any() } returns true
         every { challenge1.isSolved(any(), any(), any()) } returns true
         every { challenge1.update(any()) } returns Unit
         every { challenge1.getScore() } returns 2
         every { challenge1.isSolvable(any(), any(), any()) } returns true
         every { challenge1.printToXML(any(), any()) } returns "            <Challenge>"
-        every { challenge2.equals(any()) } returns true
+        every { challenge2 == any() } returns true
         every { challenge2.isSolved(any(), any(), any()) } returns true
         every { challenge2.update(any()) } returns Unit
         every { challenge2.getScore() } returns 3
@@ -38,120 +37,196 @@ class QuestTest : AnnotationSpec() {
         every { challenge2.printToXML(any(), any()) } returns "            <Challenge>"
     }
 
-    @AfterAll
-    fun cleanUp() {
+    afterSpec {
         unmockkAll()
     }
 
-    @Test
-    fun equals() {
-        quest.equals(null) shouldBe false
+    feature("equals") {
+        scenario("Null")
+        {
+            noStepQuest.equals(null) shouldBe false
+        }
 
-        quest.equals(Constants.Parameters()) shouldBe false
+        scenario("Not Quest")
+        {
+            noStepQuest.equals(Constants.Parameters()) shouldBe false
+        }
 
-        (quest == quest) shouldBe true
+        scenario("Self")
+        {
+            (noStepQuest == noStepQuest) shouldBe true
+        }
 
         val challenge = mockkClass(LineCoverageChallenge::class)
-        every { challenge.equals(any()) } returns true
+        every { challenge == any() } returns true
         val quest1 = Quest("Lines over Lines", arrayListOf(QuestStep("", challenge)))
-        (quest == quest1) shouldBe false
+        scenario("Different Quest")
+        {
+            (noStepQuest == quest1) shouldBe false
+        }
 
         val quest2 = Quest("Lines over Lines", arrayListOf(QuestStep("", challenge)))
-        (quest1 == quest2) shouldBe true
+        scenario("Same Steps in Quest")
+        {
+            (quest1 == quest2) shouldBe true
+        }
+
+        scenario("Quest has Subset of Steps")
+        {
+            (quest1 == noStepQuest) shouldBe false
+        }
     }
 
-    @Test
-    fun getCurrentStep() {
-        quest1.getCurrentStep() shouldBe step1
+    feature("getCurrentStep") {
+        scenario("Quest on Initial Step")
+        {
+            twoStepQuest.getCurrentStep() shouldBe step1
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns false
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.getCurrentStep() shouldBe step2
+        scenario("Challenge (first step) is solved, second is not")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.getCurrentStep() shouldBe step2
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns true
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.getCurrentStep() shouldBe step2
+        scenario("Both steps solved, Quest stays on last step")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.getCurrentStep() shouldBe step2
+        }
     }
 
-    @Test
-    fun getCurrentStepNumber() {
-        quest1.getCurrentStepNumber() shouldBe 0
+    feature("getCurrentStepNumber") {
+        scenario("Quest on Initial Step")
+        {
+            twoStepQuest.getCurrentStepNumber() shouldBe 0
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns false
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.getCurrentStepNumber() shouldBe 1
+        scenario("Challenge (first step) is solved, second is not")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.getCurrentStepNumber() shouldBe 1
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns true
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.getCurrentStepNumber() shouldBe 2
+        scenario("Both steps solved, StepNumber moved past last step")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.getCurrentStepNumber() shouldBe 2
+        }
     }
 
-    @Test
-    fun getLastStep() {
-        quest1.getLastStep() shouldBe step1
+    feature("getLastSolvedStep") {
+        scenario("No step solved yet")
+        {
+            twoStepQuest.getLastSolvedStep() shouldBe step1
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns false
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.getLastStep() shouldBe step1
+        scenario("First Step solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.getLastSolvedStep() shouldBe step1
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns true
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.getLastStep() shouldBe step2
+        scenario("All Steps solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.getLastSolvedStep() shouldBe step2
+        }
     }
 
-    @Test
-    fun getScore() {
-        quest.getScore() shouldBe 0
+    feature("getScore") {
+        scenario("Quest without steps")
+        {
+            noStepQuest.getScore() shouldBe 0
+        }
 
-        quest1.getScore() shouldBe 7
+        scenario("Quest with steps")
+        {
+            twoStepQuest.getScore() shouldBe 7
+        }
     }
 
-    @Test
-    fun isCurrentStepSolved() {
-        quest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+    feature("isCurrentStepSolved") {
+        scenario("No Steps exist")
+        {
+            noStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns false
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+        scenario("Current (First) Step solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+        }
 
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+        scenario("Current (Second) Step not solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns true
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+        scenario("Current (Second) Step solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+        }
     }
 
-    @Test
-    fun isSolvable() {
-        quest.isSolvable(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+    feature("isSolvable") {
+        scenario("Quest has no Steps to be solved")
+        {
+            noStepQuest.isSolvable(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+        }
 
-        quest1.isSolvable(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+        scenario("All Steps can be solved")
+        {
+            twoStepQuest.isSolvable(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+        }
 
         every { challenge1.isSolvable(any(), any(), any()) } returns false
-        quest1.isSolvable(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+        scenario("A Step cannot be solved")
+        {
+            twoStepQuest.isSolvable(Constants.Parameters(), run, TaskListener.NULL) shouldBe false
+        }
     }
 
-    @Test
-    fun isSolved() {
-        quest.isSolved() shouldBe false
+    feature("isSolved") {
+        scenario("Quest has no Steps")
+        {
+            noStepQuest.isSolved() shouldBe false
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns false
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.isSolved() shouldBe false
+        scenario("A Step is not solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.isSolved() shouldBe false
+        }
 
         every { challenge2.isSolved(any(), any(), any()) } returns true
-        quest1.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest1.isSolved() shouldBe true
-        quest1.solved shouldNotBe 0
+        scenario("All steps are solved")
+        {
+            twoStepQuest.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            twoStepQuest.isSolved() shouldBe true
+            twoStepQuest.solved shouldNotBe 0
+        }
 
         val quest2 = Quest("Lines over Lines", arrayListOf(step1, step2))
-        quest2.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
-        quest2.getCurrentStepNumber() shouldBe 2
-        quest2.isSolved() shouldBe true
-        quest2.solved shouldNotBe 0
+        scenario("Different Quest with same steps is solved too")
+        {
+            quest2.isCurrentStepSolved(Constants.Parameters(), run, TaskListener.NULL) shouldBe true
+            quest2.getCurrentStepNumber() shouldBe 2
+            quest2.isSolved() shouldBe true
+            quest2.solved shouldNotBe 0
+        }
     }
 
-    @Test
-    fun printToXML() {
-        val result1 = "<Quest name=\"Lines over Lines\" created=\"${quest1.created}\" solved=\"0\">\n" +
+    feature("printToXML") {
+        val result1 = "<Quest name=\"Lines over Lines\" created=\"${twoStepQuest.created}\" solved=\"0\">\n" +
                 "    <QuestSteps count=\"2\">\n" +
                 "        <QuestStep description=\"\">\n" +
                 "            <Challenge>\n" +
@@ -161,9 +236,12 @@ class QuestTest : AnnotationSpec() {
                 "        </QuestStep>\n" +
                 "    </QuestSteps>\n" +
                 "</Quest>"
-        quest1.printToXML("", "") shouldBe result1
+        scenario("No Reason, no Indentation")
+        {
+            twoStepQuest.printToXML("", "") shouldBe result1
+        }
 
-        val result2 = "<Quest name=\"Lines over Lines\" created=\"${quest1.created}\" solved=\"0\" reason=\"reason\">\n" +
+        val result2 = "<Quest name=\"Lines over Lines\" created=\"${twoStepQuest.created}\" solved=\"0\" reason=\"reason\">\n" +
                 "    <QuestSteps count=\"2\">\n" +
                 "        <QuestStep description=\"\">\n" +
                 "            <Challenge>\n" +
@@ -173,11 +251,13 @@ class QuestTest : AnnotationSpec() {
                 "        </QuestStep>\n" +
                 "    </QuestSteps>\n" +
                 "</Quest>"
-        quest1.printToXML("reason", "") shouldBe result2
+        scenario("Scenario")
+        {
+            twoStepQuest.printToXML("With Reason, no Indentation", "") shouldBe result2
+        }
     }
 
-    @Test
-    fun testToString() {
-        quest1.toString() shouldBe "Lines over Lines"
+    feature("toString") {
+        twoStepQuest.toString() shouldBe "Lines over Lines"
     }
-}
+})
